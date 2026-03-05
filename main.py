cbd155d542f10d8ce2ce625472cb20b33d6cc0d7
Lix <Lixczyy@gmail.com>
2026-03-05 11:36:26 +0100

    changed site layout

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 app= Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-app.run(debug=True)
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
